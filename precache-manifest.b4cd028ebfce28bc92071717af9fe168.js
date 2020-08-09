06e15a306064392be2a869885e475ef94960197f
Pierre Yue <pierreyue@Pierres-MBP.hitronhub.home>
2020-08-08 21:38:43 -0400

    Updates

---
+++ precache-manifest.b4cd028ebfce28bc92071717af9fe168.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e49cd74e360108a69b6d932e11ed4e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e7530f00786bdda835d6",
+    "url": "/monsters-rolodex/static/css/main.35970fc1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7530f00786bdda835d6",
+    "url": "/monsters-rolodex/static/js/main.454168ff.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
